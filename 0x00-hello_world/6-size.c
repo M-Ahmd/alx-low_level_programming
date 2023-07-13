6bf155d55865db63f73867d595fa5a90d3d9e2ee
M-Ahmd <ma0950082@gmail.com>
2023-07-13 19:58:41 +0300

    Alx

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*main- Entry point
+*Description: A c programm display size of data type
+*Return: Always 0
+*/
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
