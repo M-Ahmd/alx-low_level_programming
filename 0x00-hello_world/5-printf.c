407215d412b46a5e9059d764d91b914ceb21866d
M-Ahmd <ma0950082@gmail.com>
2023-07-13 19:30:56 +0300

    Alx

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
 *main - Entry point
 *Description: A c program print a phrase
-*Return: Always 0*/
+*Return: Always 0
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
