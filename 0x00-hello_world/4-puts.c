7bbc326ce302fd231f607276e6b4d36a1178960a
M-Ahmd <ma0950082@gmail.com>
2023-07-13 19:04:32 +0300

    Alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
 
 *Description: A c program that prints with put function
 
-*Return: Always 0*/
+*Return: Always 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
