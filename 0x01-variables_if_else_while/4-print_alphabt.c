bebbfb68b3ecd861f9800c7267d13721c7bce33f
M-Ahmd <ma0950082@gmail.com>
2023-07-14 18:09:20 +0300

    Alx

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description:A c programm do something
+*Return: Always 0
+*/
+int main(void)
+{
+	char ch[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26;)
+{
+	if (i != 4 && i != 16)
+{
+	putchar(ch[i]);
+	i++;
+}
+else
+{
+	continue;
+	i++;
+}
+}
+	putchar('\n');
+	return (0);
+}
