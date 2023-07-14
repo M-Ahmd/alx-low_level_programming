937a037f2eb8e58ebda5dccae8ffca3a64d0c24c
M-Ahmd <ma0950082@gmail.com>
2023-07-14 16:23:17 +0300

    Alx

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*Descrioption: A c programm print alphabet
+*Return: Always 0
+*/
+int main(void)
+{
+printf("abcdefghijklmnopqrstuvwxyz\n");
+return (0);
+}
