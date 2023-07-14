65b3f062b80e319ad4d945bca1aa07280f68994b
M-Ahmd <ma0950082@gmail.com>
2023-07-14 17:26:43 +0300

    Alx

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*Description: A c programm display a alphabet
+*Return: Always 0
+*/
+int main(void)
+{
+	char ch[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+	putchar(ch[i]);
+	}
+	putchar('\n');
+}
