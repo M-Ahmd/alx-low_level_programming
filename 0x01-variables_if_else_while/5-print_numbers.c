64def3f8f540f8e8e429ce9bca4bb8a3bba1e494
M-Ahmd <ma0950082@gmail.com>
2023-07-14 18:37:41 +0300

    Alx

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main- Entry point
+*Descpription: A c programm do something with numbers
+*Return: Always 0
+*/
+	int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
