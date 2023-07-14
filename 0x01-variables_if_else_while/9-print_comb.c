0714ea8f1b387bb47ccd7a4733eb7f70ec18b0d0
M-Ahmd <ma0950082@gmail.com>
2023-07-14 20:42:53 +0300

    Alx

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*Description: A c programm display a alphabet
+*Return: Always 0
+*/
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
