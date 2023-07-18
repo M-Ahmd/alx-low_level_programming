f13c03b2b314e66799fa41012b320878eed4ed31
M-Ahmd <ma0950082@gmail.com>
2023-07-18 23:25:03 +0300

    $mass

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*_islower - function
+*Description: A c traning programm for a header file
+*Return: Always 0
+*@c: the character to check
+*/
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
