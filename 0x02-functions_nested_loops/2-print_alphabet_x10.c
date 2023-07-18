da3c80b2540876d3a205f86cf877f4ab902b1f61
M-Ahmd <ma0950082@gmail.com>
2023-07-18 15:32:13 +0300

    Alx is the best

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - intry point
+*Descrioption:A c programm
+*Return:Always 0
+*/
+
+void print_alphabet_x10(void)
+{
+char i;
+int counter = 0;
+while (counter < 10)
+{
+for ( i = 'a'; i < 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+counter++;
+}
+}
