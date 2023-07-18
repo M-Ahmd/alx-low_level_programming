3c31b2753ed8368f4933f9341b43d108deff71ba
M-Ahmd <ma0950082@gmail.com>
2023-07-18 14:27:32 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,9 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,7 +17,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-int _putchar(char c){
-	putchar(c);
-	return (0);
-}
+#endif /* MAIN_H */
