ca99c0831426aaa5badd8cbf9698dec9d0d4f050
M-Ahmd <ma0950082@gmail.com>
2023-07-19 00:56:53 +0300

    $mass

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_last_digit- is a function
+*@l : is a arguement
+*Description: A c programm print the last digital
+*Return: Always 0
+*/
+int print_last_digit(int l)
+{
+	int ret = l % 10;
+
+	return (ret);
+}
