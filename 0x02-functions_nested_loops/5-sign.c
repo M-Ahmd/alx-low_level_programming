abefa9d56e303a780c665b882f0698a4f13b6949
M-Ahmd <ma0950082@gmail.com>
2023-07-19 00:17:48 +0300

    $mass

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,18 @@
 int print_sign(int n)
 {
 if (n > 0)
+{
 return (1);
 _putchar('+');
+}
 else if (n == 0)
+{
 return (0);
 _putchar('0');
+}
 else if (n < 0)
+{
 return (-1);
 _putchar('-');
+}
 }
