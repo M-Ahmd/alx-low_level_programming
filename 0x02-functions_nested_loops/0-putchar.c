0ad069278d4e321a72d2bdba8377ddadad13b301
M-Ahmd <ma0950082@gmail.com>
2023-07-18 13:51:53 +0300

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - Entery point
+*Description: A c print program
+*Return: Always 0
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+It is the 0x02. C - Functions, nested loops
