8d5a0686e50e6bfff3882f9c03905280dfbdcaf0
M-Ahmd <ma0950082@gmail.com>
2023-07-19 00:40:28 +0300

    $mass

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs - is a a function for boult value
+*@n: is a argument
+*Description:is a a programm for boult value
+*Return: Always 0
+*/
+int _abs(int n)
+{
+if (n < 0)
+return (n * -1);
+else
+return (n);
+}
