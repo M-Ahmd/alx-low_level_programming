bd09a6a51843dccb2b1c548d6e24bbaed6566e9b
M-Ahmd <ma0950082@gmail.com>
2023-07-18 15:11:56 +0300

    Alx is the best

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*main - Entry point
+*Desctiption: A c programm to train about header file
+*Return: Always 0
+*/
+int main(void)
+{
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+	_putchar(ch[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
