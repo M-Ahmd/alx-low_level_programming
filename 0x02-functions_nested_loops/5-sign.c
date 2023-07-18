93ab6d85e02aa9eb31efdb0424910a0e601e1b92
M-Ahmd <ma0950082@gmail.com>
2023-07-19 00:10:02 +0300

    $mass

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_sign - A function
+*@n:is Argument number
+*Description: A programm for print the sing of number
+*Return: Always 0
+*/
+int print_sign(int n)
+{
+if (n > 0)
+return (1);
+else if (n == 0)
+return (0);
+else
+return (-1);
+}
