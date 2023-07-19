5d88e897d206a83c686c018ad502988b4a3a6075
M-Ahmd <ma0950082@gmail.com>
2023-07-20 00:01:27 +0300

    $mass

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include main.h
+#include "main.h"
 
 /**
 * print_remaining_days - takes a date and prints how many days are
@@ -19,19 +19,19 @@ void print_remaining_days(int month, int day, int year)
 			day++;
 		}
 
-		printf(Day of the year: %dn, day);
-		printf(Remaining days: %dn, 366 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
 		if (month == 2 && day == 60)
 		{
-		printf(Invalid date: %02d/%02d/%04dn, month, day - 31, year);
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-		printf(Day of the year: %dn, day);
-		printf(Remaining days: %dn, 365 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
