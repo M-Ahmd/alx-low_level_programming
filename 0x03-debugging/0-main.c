2fec00cbf05ea92b5eef9a2176f6930e859f8bc3
M-Ahmd <ma0950082@gmail.com>
2023-07-19 23:23:26 +0300

    $mass

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
-*main - intery point
-*Description: A c programm
-*Return: Always 0
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
