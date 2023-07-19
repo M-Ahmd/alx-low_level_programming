733e58cd2032b4cbe1c0780d1c569f9292d72759
M-Ahmd <ma0950082@gmail.com>
2023-07-19 15:32:21 +0300

    Alx is the best

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,32 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
 /**
-*print_to_98 - is a function
-*@n: is a argument
-*Return : Always 0
-*/
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
 void print_to_98(int n)
 {
-	int i;
+	int i, j;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-	if (i > 10)
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
 	{
-	printf("%d", i);
-	}
-	else
-	{
-	printf("%d", i / 10);
-	printf("%d",i % 10);
-
-	}
-	if (i != 98)
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
 }
