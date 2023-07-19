52db4efd45ab675249c4fade5c3eaa40a7261ad6
M-Ahmd <ma0950082@gmail.com>
2023-07-19 14:46:19 +0300

    Alx is f

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,38 +1,38 @@
 #include "main.h"
+
 /**
-*times_table - is a void function
-*Description: it is a simple code to display times table
-*Return: Always 0
-*/
+ * times_table - is a void function
+ * Description: it is a simple code to display times table
+ * Return: Always 0
+ */
 void times_table(void)
 {
-		int i;
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        int j;
+        for (j = 0; j < 10; j++)
+        {
+            int res = j * i;
 
-	for (i = 0; i < 10; i++)
-	{
-	int j;
+            if (j != 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
 
-	for (j = 0; j < 10; j++)
-	{
-	int res = j * i;
+            if (res >= 10)
+            {
+                _putchar(res / 10 + '0');
+                _putchar(res % 10 + '0');
+            }
+            else
+            {
+                _putchar(' ');
+                _putchar(res + '0');
+            }
+        }
+        _putchar('\n');
+    }
+}
 
-	if (res > 9)
-{
-	_putchar(' ');
-	_putchar(res / 10 + '0');
-	_putchar(res % 10 + '0');
-}
-else
-{
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(res + '0');
-}
-	if (j != 9)
-	{
-	_putchar(',');
-	}
-	}
-	_putchar('\n');
-	}
-}
