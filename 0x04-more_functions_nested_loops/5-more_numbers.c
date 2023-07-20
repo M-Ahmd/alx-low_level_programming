234a7a1111ec62920c2e72b0bfd2d9d5e1c98376
M-Ahmd <ma0950082@gmail.com>
2023-07-20 16:23:44 +0300

    $mass

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*more_numbers - is a function
+*Description: A c programm prints int
+*Return: Always 0
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 11; j++)
+	{
+	for (i = 0; i < 15; i++)
+	{
+	_putchar(i + '0');
+	}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
