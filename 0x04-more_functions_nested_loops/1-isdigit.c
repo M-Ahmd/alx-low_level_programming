3e61216bfca73ddad4d39ec9ad1d1ea913290bdc
M-Ahmd <ma0950082@gmail.com>
2023-07-20 15:42:33 +0300

    $mass

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isdigit - is a function
+*@c: is passed argument
+*Return: Always 0
+*/
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	return (1);
+	else
+	return (0);
+}
