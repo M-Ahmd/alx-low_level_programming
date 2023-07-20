6620411643daded5210f34e7c5bdd505252ea304
M-Ahmd <ma0950082@gmail.com>
2023-07-20 19:41:08 +0300

    $mass

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*print_square - is a function
+*@size: is an argument
+*Return: Always 0
+*/
+void print_square(int size)
+{
+
+	int i, j;
+
+	if (size > 0)
+	{
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+
+	}
+	else
+{
+	_putchar('\n');
+}
+
+
+}
