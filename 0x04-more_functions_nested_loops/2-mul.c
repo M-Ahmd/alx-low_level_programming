e7d675d93ea8c13f9006cf9aa5035a75f04b7533
M-Ahmd <ma0950082@gmail.com>
2023-07-20 15:49:15 +0300

    $mass

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*mul - is a function return muli_of_two_int
+*@a: is argument number 1
+*@b: is argument number 2
+*Return: Always 0
+*/
+int mul(int a, int b)
+	{
+		int res = a * b;
+
+		return (res);
+	}
