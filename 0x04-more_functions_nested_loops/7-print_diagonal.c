e476dfb617ef44858be74617d30eae415a0dacba
M-Ahmd <ma0950082@gmail.com>
2023-07-20 17:38:57 +0300

    $mass

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_diagonal - is a function
+*@n: is an argument
+*Description: A c programm print diagonal
+*Return: Always 0
+*/
+void print_diagonal(int n)
+
+{
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	_putchar('\');
+	}
+	_putchar('\n');
+	}
+}
