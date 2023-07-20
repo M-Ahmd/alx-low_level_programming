66b5ac479a099ee3dfcd4334a931dac7f562fea5
M-Ahmd <ma0950082@gmail.com>
2023-07-20 15:28:25 +0300

    $mass

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isupper - is a function
+*@c : is atgument
+*Return: Always 0
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0)
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+It is the more functions projects
