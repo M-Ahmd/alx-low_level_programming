4ff58ebcd610d6a6c5f2b6ce1f04eadbf0b878c6
M-Ahmd <ma0950082@gmail.com>
2023-07-20 16:44:09 +0300

    $mass

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,17 @@
 */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, num;
 
 	for (j = 0; j < 10; j++)
 	{
 	for (i = 0; i <= 14; i++)
 	{
-	if (i < 9)
-	_putchar(i + '0');
-	else
+	if (i >= 10)
 	{
 	_putchar(i / 10 + '0');
-	_putchar(i % 10 + '0');
+	}
+	_putchar(i % 10 + '0');	
 	}
 	}
 	_putchar('\n');
