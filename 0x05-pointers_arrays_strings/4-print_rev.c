a28ba3a87ddad8727c8b52a41fa9d80c43efa459
M-Ahmd <ma0950082@gmail.com>
2023-07-24 16:21:51 +0300

    $mass

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_rev - is a function
+*@s: is an argument
+*Return: Always 0
+*/
+void print_rev(char *s)
+{
+	/*We want to get string length*/
+	int len = 0;
+	int i;
+
+	while (*(s + len) != '\0')
+	{
+	len++;
+	}
+	/*let's revese it*/
+	i = len;
+	for (i > 0; i--;)
+	{
+	_putchar (*(s + i));
+	}
+	_putchar ('\n');
+
+}
