ef108d7f1a25927cb1d1cde6b4be6299cbd9572d
M-Ahmd <ma0950082@gmail.com>
2023-07-24 20:39:35 +0300

    $mass

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,14 +17,14 @@ void puts_half(char *str)
 	}
 	if (len / 2 == 0)
 	{
-	for (i = len / 2; i > len; i++)
+	for (i = len / 2; i < len; i++)
 	{
 	_putchar(*(str + i));
 	}
 	}
 	else
 	{
-	for (i = (len - 1) / 2; i > len; i++)
+	for (i = (len - 1) / 2; i < len; i++)
 	{
 	_putchar(*(str + i));
 	}
