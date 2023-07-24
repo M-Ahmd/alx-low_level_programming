aa4fa3665f7a8645a50256de5a29e48b48fdc950
M-Ahmd <ma0950082@gmail.com>
2023-07-24 20:07:22 +0300

    $mass

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 /**
-*rev_string - is a function
-*@s: is an argument
-*Return: Always 0
-*/
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
 void rev_string(char *s)
 {
 	char tmp;
@@ -25,24 +25,4 @@ void rev_string(char *s)
 		s[i] = s[len1];
 		s[len1--] = tmp;
 	}
-	char tmp;
-	int i, len, len1;
-
-	len = 0;
-	len1 = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	len1 = len - 1;
-
-	for (i = 0; i < len / 2; i++)
-	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
-	}
-
 }
