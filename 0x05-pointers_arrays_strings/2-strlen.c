91e8c8e52238fcf27c19234379a0549532e7e1ad
M-Ahmd <ma0950082@gmail.com>
2023-07-24 15:51:41 +0300

    $mass

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,12 @@
 #include "main.h"
-
+/**
+*_strlen - is a function
+*@s: is a argument
+*Return: Always 0
+*/
 int _strlen(char *s)
 {
-int len=0;
+int len = 0;
 while (*(s + len) != '\0')
 {
 len++;
