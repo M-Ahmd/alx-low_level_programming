0a3b6e306ce5a965dd2bc139550d8d6ceb0de7c4
M-Ahmd <ma0950082@gmail.com>
2023-07-24 21:09:52 +0300

    $mass

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_array - is a funtion
+*@a: is an argument
+*@n: is an array size
+*Return: Always 0
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", *(a + i));
+	if (i != n - 1)
+	{
+	printf(",");
+	printf(" ");
+	}
+	}
+}
