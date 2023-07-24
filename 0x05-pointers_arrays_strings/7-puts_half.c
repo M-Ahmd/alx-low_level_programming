a8cc5c3e410870ab5c202a4b0e7c8803be215af8
M-Ahmd <ma0950082@gmail.com>
2023-07-24 20:37:21 +0300

    $mass

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+*puts_half - is a function
+*@str: is a grument
+*Return: Always 0
+*/
+void puts_half(char *str)
+{
+
+	/*We want to get string length*/
+	int len = 0;
+	int i;
+
+	while (*(str + len) != '\0')
+	{
+	len++;
+	}
+	if (len / 2 == 0)
+	{
+	for (i = len / 2; i > len; i++)
+	{
+	_putchar(*(str + i));
+	}
+	}
+	else
+	{
+	for (i = (len - 1) / 2; i > len; i++)
+	{
+	_putchar(*(str + i));
+	}
+
+	}
+	_putchar ('\n');
+
+
+}
