6a85f5e86bf6d933d374763f0302500fbf5cb38b
M-Ahmd <ma0950082@gmail.com>
2023-07-27 01:58:37 +0300

    $mass

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strncpy - is a function
+*@dest: is an array
+*@src: is an array too
+*@n: is an integer
+*Return: Always 0
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
+}
