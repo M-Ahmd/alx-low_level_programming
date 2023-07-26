30057e66deba60fa6599f759443e7be2795d0b22
M-Ahmd <ma0950082@gmail.com>
2023-07-27 01:33:10 +0300

    $mass

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+*_strncat - is a function
+*@dest: is an argument
+*@src: is an argument
+*@n: is an argument
+*Return: Always 0
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	/*Let is get length of each array*/
+	int len1 = 0;
+	int len2 = 0;
+	int i = 0;
+	int j = 0;
+	int counter = 0;
+
+	while (*(dest + i) != '\0')
+	{
+	len1++;
+	i++;
+	}
+	while (*(src + j) != '\0')
+	{
+	len2++;
+	j++;
+	}
+	i = 0;
+	j = 0;
+	/*PUM we now know each array length let is make them togather*/
+	for (i = 0, j = len1; i < len2; i++, j++)
+	{
+	if (counter < n)
+	{
+	dest[j] = src[i];
+	}
+	else
+	{
+	break;
+	}
+	}
+
+	/*now we did it*/
+	return (dest);
+}
