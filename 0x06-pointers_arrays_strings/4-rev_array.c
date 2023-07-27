dbe8036e9751d936646233db6653436c7c34f2ef
M-Ahmd <ma0950082@gmail.com>
2023-07-27 17:05:46 +0300

    $mass

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,19 @@
 #include "main.h"
-
+/**
+*reverse_array - is a function
+*@a: is an given array
+*@n: number of items
+*Return: Always 0
+*/
 void reverse_array(int *a, int n)
 {
 	int i;
 	int size = n - 1;
+
 	for (i = 0; i < n / 2; i++)
 	{
 	int temp = a[i];
+
 	a[i] = a[size];
 	a[size--] = temp;
 	}
