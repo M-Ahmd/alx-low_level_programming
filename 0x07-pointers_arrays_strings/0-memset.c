677311a417a4548e54774fa2a0d08c030036adec
M-Ahmd <ma0950082@gmail.com>
2023-07-31 17:55:32 +0300

    $mass

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,15 @@
 #include "main.h"
-
+/**
+**_memset - is a function
+*@s: is an argument
+*@b: is an argument
+*@n: is an argument
+*Return: Always 0
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+It is String and pointer project
