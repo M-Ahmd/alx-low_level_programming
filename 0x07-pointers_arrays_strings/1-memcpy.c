4b87c92cec9d95157941f2152ab6e532ec57d34d
M-Ahmd <ma0950082@gmail.com>
2023-07-31 18:16:57 +0300

    $mass

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,17 +8,11 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int len = 0;
 	unsigned int i;
 
-	while (*(dest + len) != '\0')
-	{
-	len++;
-	}
 	for (i = 0; i < n; i++)
 	{
-	*(dest + len) = *(src + i);
-	len++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
