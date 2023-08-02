193142ba056cf70a35ac7e556677a63808b75f1e
M-Ahmd <ma0950082@gmail.com>
2023-08-02 16:22:33 +0300

    $mass

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,10 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (*(s + i) != '\0')
+	if (*s != '\0')
 	{
-	_putchar(*(s + i));
-	_puts_recursion(*(s + i));
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	}
 	else
 	{
