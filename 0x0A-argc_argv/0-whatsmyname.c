f103d9dfe55349d806d5581dafdcab7cb2103a8f
M-Ahmd <ma0950082@gmail.com>
2023-08-04 19:53:27 +0300

    $mass

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+*main - is entry point
+*@argc: is an argument
+*@argv: is an argument too
+*Return: Always 0
+*/
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
