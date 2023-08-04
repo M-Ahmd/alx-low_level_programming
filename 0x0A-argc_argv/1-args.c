9a200e832e1d60c563f32f7c364a9306f7352146
M-Ahmd <ma0950082@gmail.com>
2023-08-04 19:59:22 +0300

    $mass

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - is entry point
+*@argc: is an argument
+*@argv: is an argument
+*Return: Always 0
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
