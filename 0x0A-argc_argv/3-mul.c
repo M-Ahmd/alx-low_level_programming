73018d91d1537faeaf5b0d6119c7638f57de1f9b
M-Ahmd <ma0950082@gmail.com>
2023-08-04 20:42:16 +0300

    $mass

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+*main - is entry point
+*@argc: is argument
+*@argv: is argument
+*Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	else
+	num1 = atoi(argv[1]);
+        num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
+        return (0);
+
+}
