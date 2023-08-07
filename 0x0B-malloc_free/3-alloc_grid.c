6706998296ec9bf6bb5a681c00f7abc91bfa63f6
M-Ahmd <ma0950082@gmail.com>
2023-08-07 15:29:46 +0300

    $mass

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*alloc_grid - is a fun
+*@width: is an argument
+*@height: is an argument
+*Return: Always 0
+*/
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+	arr[i] = malloc(sizeof(int) * width);
+	if (arr[i] == NULL)
+	{
+	for (j = 0; j < i; j++)
+	{
+	free(arr[j]);
+	}
+	free(arr);
+	}
+	for (j = 0; j < width; j++)
+	{
+	arr[i][j] = 0;
+	}
+	}
+	return (arr);
+}
