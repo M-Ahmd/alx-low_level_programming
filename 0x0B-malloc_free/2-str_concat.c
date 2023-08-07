623adc9ec93f56c04ceb967a827d7c721ea530df
M-Ahmd <ma0950082@gmail.com>
2023-08-07 13:51:39 +0300

    $mass

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*str_concat - is a fun
+*@s1: is the first string
+*@s2: is the second string
+*Return: Always 0
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, len, i, j;
+	char *ptr;
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	len = len1 + len2;
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+	ptr[i] = s1[i];
+	}
+	for (i = len1, j = 0; j < len2; i++, j++)
+	{
+	ptr[i] = s2[j];
+	}
+	ptr[len] = '\0';
+	return (ptr);
+}
