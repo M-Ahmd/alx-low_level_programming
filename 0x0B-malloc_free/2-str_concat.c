214e2f5e5711eec5c464450aec7d2ba3363e24b4
M-Ahmd <ma0950082@gmail.com>
2023-08-07 14:00:06 +0300

    $mass

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,26 @@ char *str_concat(char *s1, char *s2)
 {
 	int len1, len2, len, i, j;
 	char *ptr;
-
+	if (s1 == NULL && s1 == NULL)
+	{
+	len1 = 0;
+	len2 = 0;
+	}
+	else if (s1 == NULL && s2 != NULL)
+	{
+	len1 = 0;
+	len2 = strlen(s2);
+	}
+	else if (s1 != NULL && s2 == NULL)
+        {
+        len1 = strlen(s1);
+        len2 = 0;
+        }
+	else
+	{
 	len1 = strlen(s1);
 	len2 = strlen(s2);
+	}
 	len = len1 + len2;
 	ptr = malloc(sizeof(char) * (len + 1));
 	if (ptr == NULL)
