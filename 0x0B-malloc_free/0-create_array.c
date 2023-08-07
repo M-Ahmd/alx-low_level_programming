14ad7bdec3913cb09c1c3706ec043117d233d0a1
M-Ahmd <ma0950082@gmail.com>
2023-08-07 11:15:30 +0300

    $mass

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,17 +9,20 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	unsigned int i;
+	
 	if (size == 0)
 	{
 	return (NULL);
 	}
 	else
 	{
-	ptr = (char *) malloc(sizeof(int) * 1);
-
-	*ptr = c;
+	ptr = (char *) malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
+	{
+	*(ptr + i) = c;
+	}
 	return (ptr);
 	}
-	free(ptr);
 
 }
