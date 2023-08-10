b76b25fbb6391c8625bbb76bab95141d1e5648fe
M-Ahmd <ma0950082@gmail.com>
2023-08-10 16:39:14 +0300

    $mass

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,24 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+*_realloc - is an array
+*@ptr: is an argument
+*@old_size: is an argument
+*@new_size: is an argument
+*Return: Always 0
+*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
 	void *new_ptr;
 
 	if (ptr == NULL)
-	return malloc(new_size);
+	return (malloc(new_size));
 	if (new_size == old_size)
 	return (ptr);
-	
+
 	new_ptr = malloc(new_size);
-	
+
 	if (new_ptr == NULL)
 	{
 	return (NULL);
 	}
-	
+
 	for (i = 0; i < old_size; i++)
 	{
 	((char *)new_ptr)[i] = ((char *)ptr)[i];
