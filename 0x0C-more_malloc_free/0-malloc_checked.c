9b51c9645aa892a5c4f18da8c7f45fb69b12b649
M-Ahmd <ma0950082@gmail.com>
2023-08-10 13:55:35 +0300

    $mass

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+*malloc_checked - is a fun
+*@b: is an argument
+*Return: Always 0
+*/
 void *malloc_checked(unsigned int b)
 {
 	int *p = malloc(b);
+
 	return (p);
 }
