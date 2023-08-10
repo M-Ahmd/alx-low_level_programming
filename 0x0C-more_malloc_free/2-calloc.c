126fb894c774341d5b127ae0f85fbd030debe53d
M-Ahmd <ma0950082@gmail.com>
2023-08-10 15:15:42 +0300

    $mass

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*_calloc - is a fun
+*@nmemb: is an argument
+*@size: is an argument
+*Return: Always 0
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, *arr;
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+	if (nmemb == 0)
+	{
+	return (NULL);
+	}
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+	{
+	return (NULL);
+	}
+	while (arr[i] != '\0')
+	{
+	arr[i] = 0;
+	i++;
+	}
+	return (arr);
+}
