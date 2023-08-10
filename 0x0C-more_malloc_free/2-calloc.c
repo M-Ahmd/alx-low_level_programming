9ffd6d1ef6b44351a596e6cf3aff345532a9ba7e
M-Ahmd <ma0950082@gmail.com>
2023-08-10 15:23:17 +0300

    $mass

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	int *arr;
+	char *arr;
 
 	if (size == 0)
 	{
@@ -25,7 +25,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 	return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 	{
 	arr[i] = 0;
 	}
