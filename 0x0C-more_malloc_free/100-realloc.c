3031975c31ec8a6bb6e315395bb561cd87a10297
M-Ahmd <ma0950082@gmail.com>
2023-08-10 16:44:22 +0300

    $mass

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_ptr == NULL)
 	{
+	free(ptr);
 	return (NULL);
+	}
+	if (new_size == 0)
+	{
+	free(ptr);
+	return NULL;
 	}
 
 	for (i = 0; i < old_size; i++)
