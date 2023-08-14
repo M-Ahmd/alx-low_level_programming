422dac105030c3d047a31b2678586d99e7ac68b7
M-Ahmd <ma0950082@gmail.com>
2023-08-14 14:01:56 +0300

    $mass

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,7 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = strdup(name);
+	d->name = name;
 	d->age = age;
-	d->owner = strdup(owner);
+	d->owner = owner;
 }
