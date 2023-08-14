d9765e8b72dc731f889dfbd510c6b3c8e2549ee8
M-Ahmd <ma0950082@gmail.com>
2023-08-14 13:44:06 +0300

    $mass

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+it is the project for a struct and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_dog
+#define dog_dog
+#include <stdio.h>
+/**
+*struct dog - is a struct
+*@name: is a name of dog
+*@owner: is a owner of the dog
+*@age: is a age of dog
+*Return: Always 0
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
