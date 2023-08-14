00a8e3f9bd07ef6d1f6391a1052f4acf30bfea7d
M-Ahmd <ma0950082@gmail.com>
2023-08-14 14:30:15 +0300

    $mass

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,6 +8,9 @@
 */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
+
 	if (d->name == NULL)
 	{
 	d->name = "nil";
@@ -16,10 +19,7 @@ void print_dog(struct dog *d)
 	{
 	d->owner = "nil";
 	}
-	if (d != NULL)
-	{
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
-	}
 }
