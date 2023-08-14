bc5c0ebbd2fedd90b627151569232a43e29e4ad7
M-Ahmd <ma0950082@gmail.com>
2023-08-14 14:00:46 +0300

    $mass

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*init_dog - is a function
+*@d: is an argument
+*@name: is an argument
+*@age: is an argument
+*@owner: is an argument
+*Return: Always 0
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = strdup(name);
+	d->age = age;
+	d->owner = strdup(owner);
+}
