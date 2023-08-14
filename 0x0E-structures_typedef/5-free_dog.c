86649fab29c7dce2e471f1fca0480e1b607e4d2f
M-Ahmd <ma0950082@gmail.com>
2023-08-14 15:14:08 +0300

    $mass

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * free_dog - ...
+ * @d: ...
+ *
+ * Return: ...
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+
+
+}
