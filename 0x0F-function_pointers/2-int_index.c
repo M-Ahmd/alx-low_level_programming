1c6b78277dbed9efbf451d679996fcb277d421dc
M-Ahmd <ma0950082@gmail.com>
2023-08-16 12:43:53 +0300

    $mass

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,23 +8,18 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, index = 0;
+	int i;
 
 	if (array == NULL || cmp == NULL)
 	{
 	return (-1);
 	}
 	if (size <= 0)
-	{
 	return (-1);
-	}
 	for (i = 0; i < size; i++)
 	{
-	if (cmp(array[i]) != 0)
-	{
+	if (cmp(array[i]) == 1)
 	return (i);
-	break;
-	}
 	}
 
 	return (-1);
