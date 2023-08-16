b57b64cfd629b084dd03d11b3800789d4ae1ad43
M-Ahmd <ma0950082@gmail.com>
2023-08-16 13:23:53 +0300

    $mass

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+*op_add - .........
+*@a: is an argument
+*@b: is an argument
+*Return: Always 0
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+*op_sub - .........
+*@a: is an argument
+*@b: is an argument
+*Return: Always 0
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+*op_mul - .........
+*@a: is an argument
+*@b: is an argument
+*Return: Always 0
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+*op_div - .........
+*@a: is an argument
+*@b: is an argument
+*Return: Always 0
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+*op_mod - .........
+*@a: is an argument
+*@b: is an argument
+*Return: Always 0
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
