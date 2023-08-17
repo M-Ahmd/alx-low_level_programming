22b91ac5d0c497a39fcd1f6821b46de677f434fb
M-Ahmd <ma0950082@gmail.com>
2023-08-17 14:13:07 +0300

    $mass

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*print_numbers - .........
+*@separator: .............
+*@n:......................
+*Return:.................
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, j = 0;
+	va_list ptr;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+	vprintf("%d", ptr);
+	if (i == n - 1)
+	break;
+	while (separator[j] != '\0')
+	{
+	putchar(separator[j]);
+	j++;
+	}
+	j = 0;
+	}
+	putchar('\n');
+	va_end(ptr);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,5 +2,5 @@
 #define MA
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
