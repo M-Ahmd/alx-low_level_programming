c33f33b42b36534f115d75729141705a60fbf79a
M-Ahmd <ma0950082@gmail.com>
2023-08-29 15:22:29 +0300

    $mass

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,9 +9,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (!head)
+	{
+	return (NULL);
+	}
 	for (i = 0; i < index; i++)
 	{
 	head = head->next;
+
 	}
 	return (head ? head : NULL);
 }
