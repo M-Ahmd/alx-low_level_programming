4da33e2ec21dcae434035b4afdbe00b4e7576e38
M-Ahmd <ma0950082@gmail.com>
2023-08-29 14:06:45 +0300

    $mass

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+*add_nodeint - .................................
+*@n:............................................
+*@head:.........................................
+*Return:........................................
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+{
+	return (NULL);
+}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
