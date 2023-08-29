65741855895b6386697c418e25202920b751b1e2
M-Ahmd <ma0950082@gmail.com>
2023-08-29 14:23:51 +0300

    $mass

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+*add_nodeint_end - ......................................
+*@head:..................................................
+*@n:.....................................................
+*Return:................................................
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *tmp;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+	*head = new_node;
+	return (new_node);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+	tmp = tmp->next;
+
+	new_node->next = tmp;
+	return (new_node);
+}
