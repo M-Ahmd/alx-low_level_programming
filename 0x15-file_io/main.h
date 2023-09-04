3baed265f2f1b375b22a051c93435940d0baf235
M-Ahmd <ma0950082@gmail.com>
2023-09-04 14:52:19 +0300

    $mass

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MA
+#define MA
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
